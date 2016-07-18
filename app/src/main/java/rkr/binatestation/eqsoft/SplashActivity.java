f1570d1c27c24760e0ac8622d24b1115e9f9b131
Raghukrishnan R <rkrsmail@gmail.com>
2016-07-18 23:15:51 +0530

    Initial Commit

---
+++ app/src/main/java/rkr/binatestation/eqsoft/SplashActivity.java
@@ -0,0 +1,28 @@
+package rkr.binatestation.eqsoft;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        startActivity(new Intent(getBaseContext(), LoginActivity.class));
+        finish();
+    }
+}
