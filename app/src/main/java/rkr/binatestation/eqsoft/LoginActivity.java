1330c35b208da06919c9acdca75f8db5a69868b5
Raghukrishnan R <rkrsmail@gmail.com>
2016-07-19 22:33:46 +0530

    Dashbord activity added

--- app/src/main/java/rkr/binatestation/eqsoft/LoginActivity.java
+++ app/src/main/java/rkr/binatestation/eqsoft/LoginActivity.java
@@ -1,7 +1,9 @@
 package rkr.binatestation.eqsoft;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(getBaseContext(), HomeActivity.class));
+        finish();
     }
 }
